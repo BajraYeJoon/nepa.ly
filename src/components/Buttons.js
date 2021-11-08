bf4169a8c1dbf566c9deb21c8f979048621b1c94
BajraYeJoon <binaya.bajrashakya@gmail.com>
2021-11-08 19:38:16 +0545

    Added ReadMe/ Enhanced Ui

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,7 +3,7 @@ import "./Buttons.css";
 
 const Buttons = ({ imp, text, link }) => {
   return (
-    <div className={`button ${imp == "secondary" ? "button__white" : ""}`}>
+    <div className={`button ${imp === "secondary" ? "button__white" : ""}`}>
       <a href={link}>{text}</a>
     </div>
   );
